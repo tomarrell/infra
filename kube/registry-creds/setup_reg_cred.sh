93cabb05190a6ebd1b5e0aca84831024be19822b
Tom Arrell <tom.arrell@gmail.com>
2020-05-28 00:56:51 +0200

    Add registry secret creation script

---
+++ kube/registry-creds/setup_reg_cred.sh
@@ -0,0 +1,5 @@
+kubectl --context personal create secret docker-registry regcred \
+  --docker-server=https://index.docker.io/v1/ \
+  --docker-username=tomarrell \
+  --docker-password=$1 \
+  --docker-email=tom.arrell@gmail.com
